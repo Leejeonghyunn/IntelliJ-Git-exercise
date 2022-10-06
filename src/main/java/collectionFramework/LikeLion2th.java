024b2d51a6c5fa98a7f7bd7ef884f678d21bf2ad
jeong hyun lee <ksy0214k@naver.com>
2022-10-06 09:25:10 +0900

    업데이트3

--- src/main/java/collectionFramework/LikeLion2th.java
+++ src/main/java/collectionFramework/LikeLion2th.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 /**
  * 멋사자 2기 학생의 이름이 들어있는 list를 return하는 메서드
+ * 1. List를 생성하고 우리반 90명의 이름을 모두 넣는다.
+ * 2. List를 생성하고 우리반 90명의 이름, 반, 깃 주소 3가지를 Student에 담아서 List에 넣는다
  */
 public class LikeLion2th {
 
-    private List<String> students = new ArrayList<>();
+    private List<String> students = new ArrayList<>(); //List 생성
+    //멤버변수는 외부에서 접근할 수 없게 private로 걸어준다.
 
-    public LikeLion2th() {
+    public LikeLion2th() { //생성자
         this.students.add("강동연");
         this.students.add("강수빈");
     }
--- src/main/java/collectionFramework/ListPracticeMain.java
+++ src/main/java/collectionFramework/ListPracticeMain.java
@@ -8,8 +8,13 @@ public class ListPracticeMain {
         LikeLion2th likeLion2th = new LikeLion2th();
         List<String> students = likeLion2th.getStudentList();
 
+        //
+
+        //for each
         for (String student : students) {
             System.out.println(student);
         }
+        //list에 들어있는 element 개수 세기
+        System.out.println(students.size());
     }
 }
